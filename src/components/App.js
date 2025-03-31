03f69445f19ef4b5a320183198a68a0d1d6f0c85
ayush-pandey047 <23417dp@gmail.com>
2025-04-01 00:07:18 +0530

    add new features

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import './App.css';
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
+  const Local_Storage_Key = "contacts";
+  const [contacts,setContacts] = useState([]);
 
-  const contacts = [
-    {
-      id:"1",
-      name:"Ayush",
-      email:"123@gmail.com"
-    },
-    {
-      id:"2",
-      name:"Deepika",
-      email:"321@gmail.com"
-    }
-  ]
+  const addContactHandeler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, contact]);
+  }
+
+  useEffect(()=>{
+    const retrivecontacts = JSON.parse(localStorage.getItem(Local_Storage_Key));
+    if (retrivecontacts) setContacts(retrivecontacts)
+  },[]);
+
+  useEffect(()=>{
+    localStorage.setItem(Local_Storage_Key, JSON.stringify(contacts))
+  },[contacts]);
 
   return (
     <div className="ui container">
       <Header/>
-      <AddContact/>
+      <AddContact addContactHandeler = {addContactHandeler}/>
       <ContactList  contacts={contacts}/>
 
     </div>
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,10 +1,17 @@
 import React from "react";
+import ContactCard from "./ContactCard";
 
 const ContactList = (props) => {
-    console.log(props)
+    console.log(props);
+
+    const renderContactList = props.contacts.map((contact) => {
+         return (
+             <ContactCard contact={contact}></ContactCard>
+        );
+    })
     return(
-        <div className="ui cellied list">
-            Contact List
+        <div className="ui celled list">
+            {renderContactList}
         </div>
     )
 }
